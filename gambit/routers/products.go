8a39dbfb04e17c43720f71e1f540f3287873e240
Matias <matias.martinez90@gmail.com>
2023-08-23 21:09:46 -0300

    Siguiente Commit

--- gambit/routers/products.go
+++ gambit/routers/products.go
@@ -39,3 +39,32 @@ func InsertProduct(body string, User string) (int, string) {
 	return 200, "{ProdID: " + strconv.Itoa(int(result)) + "}"
 
 }
+
+func UpdateProduct(body string, User string, id int) (int, string) {
+	fmt.Println("Inicializando funcion  router.UpdateProduct")
+
+	var t models.Product
+
+	err := json.Unmarshal([]byte(body), &t)
+	if err != nil {
+		return 400, "Error en los datos recibidos" + err.Error()
+	}
+
+	if len(t.ProdTitle) == 0 {
+		return 400, "Debe especificar el nombre del producto "
+	}
+
+	isAdmin, msg := bd.UserIsAdmin(User)
+	if !isAdmin {
+		return 400, msg
+	}
+
+	t.ProdId = id
+	err2 := bd.UpdateProduct(t)
+	if err2 != nil {
+		return 400, "Ocurrio un error al intentar actualizar el registro de producto" + strconv.Itoa(id) + " > " + err2.Error()
+	}
+
+	return 200, " Update OK"
+
+}
