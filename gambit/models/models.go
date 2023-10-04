3ef4f0722a9ced4b2f7d6b124e43386f7730b8fe
Matias <matias.martinez90@gmail.com>
2023-10-03 21:08:23 -0300

    Siguiente Commit

--- gambit/models/models.go
+++ gambit/models/models.go
@@ -81,3 +81,24 @@ type OrdersDetails struct {
 	OD_Quantity int     `json:"odQuantity"`
 	OD_Price    float64 `json:"odPrice"`
 }
+package models
+
+type SecretRDSJson struct {
+	Username            string `json:"username"` // Alt izq + 96 // Se usa para pasar a minuscula
+	Password            string `json:"password"`
+	Engine              string `json:"engine"`
+	Host                string `json:"host"`
+	Port                int    `json:"port"`
+	DbClusterIdentifier string `json:"dbClusterIdentifier"`
+}
+
+type SignUp struct {
+	UserEmail string `json:"UserEmail"`
+	UserUUID  string `json:"UserUUID"`
+}
+
+type Category struct {
+	CategID   int    `json:"categID"`
+	CategName string `json:"categName"`
+	CategPath string `json:"categPath"`
+}
